787d108e58d95a0693ffa47b555537be6a88f353
Anthooo <anthony.menet@gmail.com>
2017-01-03 17:13:26 +0100

    algo

--- src/ExamBlancBundle/Controller/AlgoController.php
+++ src/ExamBlancBundle/Controller/AlgoController.php
@@ -13,14 +13,36 @@ class AlgoController extends Controller
     // Exercice 1
     public function number_of_char($str)
     {
+        $point = str_replace(".", "-", $str);
+        $tab = preg_replace('/([^.a-z0-9]+)/i',"",$point);
 
+        $tab2 = str_split(strtolower($tab));
 
+        $result = array_count_values($tab2);
+
+        return($result);
     }
 
     // Exercice 2
     public function panier($tab)
     {
 
+
+        $ht = 0;
+
+        foreach ($tab as $key => $value)
+        {
+            if ($value['qty']>=3) {
+                $ht = ($ht + ($value['price_ht'] * $value['qty'] * 0.95));
+            }
+            elseif ($value['qty']>=10) {
+                $ht = (($ht + $value['price_ht'] * $value['qty'] * 0.90));
+            }
+            else {
+                $ht = ($ht + $value['price_ht'] * $value['qty']);
+            }
+                print_r($ht);
+        }
 
     }
 }
