bf4f5ba802f4f5c731d56dba59046d9d76c45f98
acrobat22 <acrobat22@gmail.com>
2017-01-03 17:25:06 +0100

    2em algo, erreur dans les calculs, revoir code

--- src/ExamBlancBundle/Controller/AlgoController.php
+++ src/ExamBlancBundle/Controller/AlgoController.php
@@ -38,6 +38,63 @@ class AlgoController extends Controller
     // Exercice 2
     public function panier($tab)
     {
+        	$ht_5 = 0;
+	$ht_20 = 0;
+	$discount = 0;
+	$prixAvantRemise = 0;
+	$prixApresRemise = 0;
+	foreach ($tab as $key => $val)
+    {
+	   // Gestion produit à TVA = 5,5%
+	   if ($val['qty'] >= 3 and $val['qty'] < 10 and $val['code_tva'] = 1) {
+	   		$prixApresRemise = (($val['qty']* $val['price_ht'])*0.95);
+			$prixAvantRemise = ($val['qty']* $val['price_ht']);
+	   		$ht_5 = $ht_5 + $prixApresRemise;
+	   		$discount = $discount + ($prixAvantRemise - $prixApresRemise);
+	   }
+	   elseif ($val['qty'] >= 10 and $val['code_tva'] = 1) {
+	   		$prixApresRemise = (($val['qty']* $val['price_ht'])*0.90);
+			$prixAvantRemise = ($val['qty']* $val['price_ht']);
+	   		$ht_5 = $ht_5 + $prixApresRemise;
+	   		$discount = $discount + ($prixAvantRemise - $prixApresRemise);
+	   }
+	   // Gestion produit à TVA = 20%
+	   elseif ($val['qty'] >= 3 and $val['qty'] < 10 and $val['code_tva'] = 2) {
+	   		$prixApresRemise = (($val['qty']* $val['price_ht'])*0.95);
+			$prixAvantRemise = ($val['qty']* $val['price_ht']);
+	   		$ht_20 = $ht_20 + $prixApresRemise;
+	   		$discount = $discount + ($prixAvantRemise - $prixApresRemise);
+	   }
+	   elseif ($val['qty'] >= 10 and $val['code_tva'] = 2) {
+	   		$prixApresRemise = (($val['qty']* $val['price_ht'])*0.90);
+			$prixAvantRemise = ($val['qty']* $val['price_ht']);
+	   		$ht_20 = $ht_20 + $prixApresRemise;
+	   		$discount = $discount + ($prixAvantRemise - $prixApresRemise);
+	   }
+	   elseif ($val['code_tva'] = 1) {
+	   		$ht_5 = $ht_5 + (($val['qty']* $val['price_ht']));
+	   }
+	   elseif ($val['code_tva'] = 2) {
+	   		$ht_20 = $ht_20 + (($val['qty']* $val['price_ht']));
+	   }
+
+	}
+
+	$ht = $ht_5 + $ht_20;
+	$tva5 = $ht_5 * 0.05;
+	$tva20 = $ht_20 * 0.20;
+	$ttc = $ht + $tva5 + $tva20;
+
+	$arraySortie =
+        array(
+            'total_ht' => $ht,
+            'total_discount' => $discount,
+            'total_tva5' => $tva5,
+            'total_tva20' => $tva20,
+            'total_ttc' => $ttc
+        );
+
+	return $arraySortie;
 
 
     }
