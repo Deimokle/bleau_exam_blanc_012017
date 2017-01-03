e4a7b480f41420f0f8d53ada6226e4eedfc242bf
bruno-bouvet <bouvet.bruno@gmail.com>
2017-01-03 18:10:33 +0100

    rien a regarder

--- src/ExamBlancBundle/Controller/AlgoController.php
+++ src/ExamBlancBundle/Controller/AlgoController.php
@@ -11,13 +11,34 @@ class AlgoController extends Controller
     //////////////////////////////////////
 
     // Exercice 1
-    public function number_of_char($str)
+
+    public function number_of_char($phrase)
     {
+        $phrase = "Seul, on va plus vite, ensemble, on va plus loin";
+
+        $clear = trim(preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode(html_entity_decode(strip_tags($phrase))))));
 
 
+        $newstr = str_split($phrase);
+
+        echo $newstr;
+
+        foreach ($newstr as $value) {
+
+            $value = asort($newstr);
+//        for ($i = 0; $i < count($newstr); $i++) {
+//            $trie[] = arsort($newstr, SORT_NUMERIC);
+
+        }
+
+
+        return $value;
     }
 
-    // Exercice 2
+        // Exercice 2
+
+
+
     public function panier($tab)
     {
 
