4af1e1d3b94a46fce73cb1e2343111923154a864
acrobat22 <acrobat22@gmail.com>
2017-01-03 18:35:26 +0100

    partie symfony, pas fini, plusieurs requetes dans DefaultController

---
+++ src/TravelBundle/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TravelBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $db=$this->get('doctrine.dbal.default_connection');//on récupére un objet Doctrine\DBAL\Connection
+        $rooms=$db->fetchAll('SELECT * FROM room');
+
+
+//        Exemple de requete :
+//        SELECT * FROM `room` JOIN hotel ON hotel_id = hotel.id
+//
+//         SELECT * FROM `room` JOIN hotel ON hotel_id = hotel.id where book = 1
+//
+//          select hotel_id, count(id) from room WHERE book = 1 group by hotel_id
+//
+//          select hotel.name, count(room.id) from room, hotel where room.hotel_id = hotel.id group by hotel_id
+//
+//          select hotel.name, count(room.id) from room, hotel where room.hotel_id = hotel.id and room.book = 0 group by hotel_id
+
+        return $this->render('@Travel/Default/index.html.twig',array('rooms'=>$rooms));
+    }
+}
+
+
