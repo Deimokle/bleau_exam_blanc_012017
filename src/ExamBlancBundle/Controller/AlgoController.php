2e31afec7542b151edeb54c405c86196cfaebaaf
acrobat22 <acrobat22@gmail.com>
2017-01-03 15:42:07 +0100

    fin exo 1 algo

--- src/ExamBlancBundle/Controller/AlgoController.php
+++ src/ExamBlancBundle/Controller/AlgoController.php
@@ -13,7 +13,25 @@ class AlgoController extends Controller
     // Exercice 1
     public function number_of_char($str)
     {
+        $chaine = $str;
+        // Transforme la chaine en minuscule et conserve les caractères alpha
+        $chaine =$s = strtolower(preg_replace('/[^a-z]+/i', '', $chaine));
 
+        // Compte le nb de fois que le caractère est présent dans la chaine
+        $arraySortie = array();
+        foreach (count_chars($chaine, 1) as $i => $val)
+        {
+           $arraySortie[chr($i)] = $val;
+        }
+
+        // Classe le tab $arraySortie
+        $arraySortiTri = array();
+        arsort($arraySortie);
+        foreach ($arraySortie as $key => $val) {
+            $arraySortiTri[$key] = $val;
+        }
+
+        return $arraySortiTri;
 
     }
 
