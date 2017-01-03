8d584cbc6836bb7808322b12ebb1df078d011ccd
juliengrima <gogrinformatique@gmail.com>
2017-01-03 18:37:46 +0100

    affichage liste index ok et par hotel ok

--- src/TravelBundle/Controller/SearchController.php
+++ src/TravelBundle/Controller/SearchController.php
@@ -12,37 +12,57 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use TravelBundle\Entity\Hotel;
 use TravelBundle\Entity\Room;
+use Doctrine\DBAL\Driver\PDOConnection;
+use mysqli_driver;
+use Doctrine\ORM\Query\Expr;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
+use PDO;
 
-class SearchController
+class SearchController extends Controller
 {
 
+/* --------------------------- APPEL AFFICHAGE INDEX MAIS SERT A RIEN CAR DEFAULT INDEX EST UTILISE ---------------------- */
+//    public function indexAction(){
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $hotel = $em->getRepository('TravelBundle:Hotel')->findAll();
+//
+//        return $this->render('@Travel/Default/index.html.twig', array(
+//            'hotel' => $hotel,
+//        ));
+//    }
 
-    public function indexAction(){
-        $em = $this->getDoctrine()->getManager();
 
-        $hotel = $em->getRepository('TravelBundle:Hotel')->findAll();
-
-        return $this->render('@Travel/Default/index.html.twig', array(
-            'hotel' => $hotel,
+/* --------------------------- RECHERCHE DES HOTELS PAR ETOILES ---------------------- */
+    public function starsAction(){
+        $stars = $_GET['stars'];
+        $bdd = new PDO('mysql:host=localhost;dbname=examblanc;charset=utf8', 'root', 'root');
+        $reponse = $bdd->prepare("SELECT name FROM hotel WHERE stars = :stars");
+        $reponse->execute(array(
+            'stars' => $stars
         ));
-    }
-
-    public function starsAction($stars){
-        $em = $this->getDoctrine()->getManager();
-
-        $hotel = $em->getRepository('TravelBundle:Hotel')->findOneBy($stars);
 
         return $this->render('@Travel/Default/stars.html.twig', array(
             'hotel' => $hotel,
         ));
     }
 
+/* --------------------------- AFFICHE UN HOTEL PAR L'ID ---------------------- */
     public function hotelAction(){
 
         $id = $_GET['id'];
         $bdd = new PDO('mysql:host=localhost;dbname=examblanc;charset=utf8', 'root', 'root');
-        $reponse = $bdd->prepare("SELECT * FROM hotel WHERE id = $id");
-        $reponse->execute(array("$_GET[$id]"));
+        $reponse = $bdd->prepare("SELECT * 
+                                  FROM hotel 
+                                  WHERE id = :id");
+//        $reponse = $bdd->prepare("SELECT *
+//                                  FROM hotel, room
+//                                  WHERE hotel.id = room.hotel_id
+//                                  and id = :id");
+
+        $reponse->execute(array(
+            'id' => $id
+        ));
 
         $donnees = $reponse->fetch();
 
